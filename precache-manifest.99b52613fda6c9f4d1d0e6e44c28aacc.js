e4d6874d397c670e359e6efa82b915841726f483
Jian Chen <35271746+legendaryC@users.noreply.github.com>
2020-04-14 21:56:15 -0500

    Updates

---
+++ precache-manifest.99b52613fda6c9f4d1d0e6e44c28aacc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b2435c3859bddec6a54af507f03107",
+    "url": "/healthcareSocialNetwork/index.html"
+  },
+  {
+    "revision": "f819eae716e589700ac7",
+    "url": "/healthcareSocialNetwork/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a43d82790d0b9668125a",
+    "url": "/healthcareSocialNetwork/static/js/2.3b72b5a9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/healthcareSocialNetwork/static/js/2.3b72b5a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f819eae716e589700ac7",
+    "url": "/healthcareSocialNetwork/static/js/main.23f68bea.chunk.js"
+  },
+  {
+    "revision": "4d59c1bccb7042dc3983",
+    "url": "/healthcareSocialNetwork/static/js/runtime-main.4a4cf586.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/healthcareSocialNetwork/static/media/logo.5d5d9eef.svg"
+  }
+]);
